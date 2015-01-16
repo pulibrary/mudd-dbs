16dc22e1bdb6af32d59e8677fbe3023ccc70a011
Shaun Ellis <shaun@sdellis.com>
2015-01-16 15:57:48 -0500

    load data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Seeding from CSV file:
-FastSeeder.seed_csv!(Alumn, "ALUMNI.csv", :box, :fname, :lname, :year, :graduate, :photos, :oversize)
+# Seeding from CSV file placed in the db/seeds directory:
+
+def delete_alumni
+  Alumn.delete_all
+end
+
+def load_alumni
+  FastSeeder.seed_csv!(Alumn, "ALUMNI.csv", :box, :fname, :lname, :year, :graduate, :photos, :oversize)
+end
+
+def reset_alumni
+  delete_alumni
+  load_alumni
+end
+
+reset_alumni
